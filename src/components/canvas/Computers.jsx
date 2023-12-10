e2361645e223102aedff95dc1ab4b9945d8d7e83
KenTokyo <omed.hash@outlook.de>
2023-12-10 15:34:21 +0100

    fixed 2

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import { Suspense, useState, useEffect } from "react";
 
 import { Canvas } from "@react-three/fiber";
@@ -9,19 +10,10 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={0.5}
-        castShadow
-        shadow-mapSize={1024}
-      />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 0.75}
+        scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
